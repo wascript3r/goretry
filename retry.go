5a93bc5976f4a556a8dd0441d9c7d31321c966c4
WaScript3r <wascript3r@gmail.com>
2020-02-23 19:38:11 +0200

    fixed bugs

--- retry.go
+++ retry.go
@@ -2,42 +2,90 @@ package goretry
 
 import (
 	"context"
+	"errors"
+	"fmt"
 	"time"
 )
 
+var (
+	ErrInterrupted      = errors.New("retry function was interrupted by context")
+	ErrAttemptsExceeded = errors.New("attempts limit is exceeded")
+	ErrStopped          = errors.New("retry function was stopped")
+)
+
+type StopError struct {
+	Err error
+}
+
+func (s *StopError) Error() string {
+	if s.Err == nil {
+		return ErrStopped.Error()
+	}
+	return fmt.Sprintf("%v: %v", ErrStopped, s.Err)
+}
+
 type RetryFunc func() error
 
-func Do(ctx context.Context, f RetryFunc, intv time.Duration, now bool) {
-	go retry(ctx, f, intv, now)
+func Do(ctx context.Context, f RetryFunc, intv time.Duration, attempts int, now bool) {
+	go retry(ctx, f, intv, attempts, now)
 }
 
-func DoBlock(ctx context.Context, f RetryFunc, intv time.Duration, now bool) {
-	retry(ctx, f, intv, now)
+func DoBlock(ctx context.Context, f RetryFunc, intv time.Duration, attempts int, now bool) error {
+	return retry(ctx, f, intv, attempts, now)
 }
 
-func retry(ctx context.Context, f RetryFunc, intv time.Duration, now bool) {
-	if ctx.Err() == context.Canceled {
-		return
+func StopErr(err error) error {
+	return &StopError{err}
+}
+
+func ctxErr(ctx context.Context) error {
+	if ctx.Err() != nil {
+		return ErrInterrupted
+	}
+	return nil
+}
+
+func retry(ctx context.Context, f RetryFunc, intv time.Duration, attempts int, now bool) error {
+	if e := ctxErr(ctx); e != nil {
+		return e
+	}
+
+	callf := func() (error, bool) {
+		err := f()
+		if err == nil {
+			return ctxErr(ctx), true
+		}
+		if _, ok := err.(*StopError); ok {
+			return err, true
+		}
+		if e := ctxErr(ctx); e != nil {
+			return e, true
+		}
+		return err, false
 	}
 
 	if now {
-		if err := f(); err == nil {
-			return
+		err, exit := callf()
+		if exit {
+			return err
 		}
 	}
 	t := time.NewTimer(intv)
 
-	for {
+	for i := attempts; i > 0; i-- {
 		select {
 		case <-ctx.Done():
 			t.Stop()
-			return
+			return ErrInterrupted
 
 		case <-t.C:
-			if err := f(); err == nil {
-				return
+			err, exit := callf()
+			if exit {
+				return err
 			}
 			t.Reset(intv)
 		}
 	}
+
+	return ErrAttemptsExceeded
 }
